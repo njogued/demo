1273a47790de1ffaeb9a69cc000bd071a4724eed
njogued <njogued@gmail.com>
2023-10-03 10:17:39 +0300

    creating a database table using sequelize in express

--- models/user.js
+++ models/user.js
@@ -1,6 +1,52 @@
 // File for user models
 
-const { DataTypes } = require("sequelize");
-const { sequelize } = require(".");
+const { Sequelize, DataTypes } = require("sequelize");
+const dbConfig = require("../config/db-config");
 
-module.exports = (sequelize, DataTypes) => {};
+const sequelize = new Sequelize(
+  dbConfig.DATABASE,
+  dbConfig.USER,
+  dbConfig.PASSWORD,
+  {
+    host: dbConfig.HOST,
+    dialect: dbConfig.DIALECT,
+  }
+);
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((error) => {
+    console.error("Unable to connect to the database: ", error);
+  });
+
+const User = sequelize.define("users", {
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  phone_number: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  level: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+});
+
+sequelize
+  .sync()
+  .then(() => {
+    console.log("User table created");
+  })
+  .catch((error) => {
+    console.log("Error: ", error);
+  });
