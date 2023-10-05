b0648aa138187bf6f5b51410ae6e885350c0d8ed
njogued <njogued@gmail.com>
2023-10-05 12:35:36 +0300

    TDD

---
+++ test/user.test.js
@@ -0,0 +1,9 @@
+const chai = require("chai");
+const { Sequelize } = require("sequelize");
+const { clearDatabase } = require("sequelize-test-helpers");
+const { User } = require("../models/user");
+const assert = require("assert");
+
+describe("test the user model", () => {
+  it("should ensure that user is created", () => {});
+});
