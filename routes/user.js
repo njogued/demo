afd520a9e737ad8aaf46b133ac2a8a10603d1555
njogued <njogued@gmail.com>
2023-10-06 21:36:27 +0300

    taking in user input in signup

--- routes/user.js
+++ routes/user.js
@@ -9,6 +9,18 @@ router.get("/signup", (req, res) => {
   res.render("signup");
 });
 
+router.post("/signup", (req, res) => {
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
+  const password = req.body.password;
+  const phone_number = req.body.phone_number;
+  const level = req.body.level;
+  const county = req.body.county;
+  const constituency = req.body.constituency;
+  const ward = req.body.ward;
+});
+
 router.route(":/userName").get((req, res) => {
   res.send(`User with userName: ${req.params.userName}`);
 });
